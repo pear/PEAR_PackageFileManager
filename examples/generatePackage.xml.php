8f3d4d77847033a9e0373e40d4443cf551b60bf2
Greg Beaver <cellog@php.net>
2003-07-25 02:52:27 +0000

    Moving back to head, getting weird cvs errors
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager/trunk@135887 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/generatePackage.xml.php
@@ -0,0 +1,21 @@
+<?php
+require_once('PEAR/PackageFileManager.php');
+$test = new PEAR_PackageFileManager;
+if (PEAR::isError($test)) {
+    echo $test->getMessage();
+    exit;
+}
+$test->setOptions(
+array('baseinstalldir' => 'PEAR',
+'version' => '0.1',
+'packagedirectory' => 'C:/cvsstuff/pear/pear_packagefilemanager',
+'state' => 'alpha',
+'filelistgenerator' => 'file',
+'notes' => 'First release of PEAR_PackageFileManager',
+'ignore' => array('package.xml')));
+$test->addDependency('PEAR', '1.1');
+$e = $test->writePackageFile();
+if (PEAR::isError($e)) {
+    echo $e->getMessage();
+}
+?>
