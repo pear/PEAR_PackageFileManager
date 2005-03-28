bcd994b9ba766fec85a9a3bc06da3b8cb7205a61
Greg Beaver <cellog@php.net>
2005-03-28 03:38:25 +0000

    more conversion of phpunit -> phpt
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager/trunk@183012 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/testsuite.php
+++ tests/testsuite.php
@@ -57,7 +57,6 @@ $title = 'PhpUnit test run, PEAR_PackageFileManager package';
     	    'PEAR_PackageFileManager_TestCase_addMaintainer',
     	    'PEAR_PackageFileManager_TestCase_addConfigureOption',
     	    'PEAR_PackageFileManager_TestCase_addDependency',
-            'PEAR_PackageFileManager_File_TestCase_setupIgnore',
             'PEAR_PackageFileManager_File_TestCase_setupDirs',
             'PEAR_PackageFileManager_File_TestCase_setDir',
             'PEAR_PackageFileManager_XMLOutput_TestCase_1',
