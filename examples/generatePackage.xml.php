4f5ebdc51524328285cab80ea5cd94d048b61107
Laurent Laville <farell@php.net>
2006-04-23 08:30:36 +0000

    - add header comment block - add missing stuff that make this example ready to works
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager/trunk@211876 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/generatePackage.xml.php
+++ examples/generatePackage.xml.php
@@ -1,28 +1,34 @@
 <?php
 /**
  * A simple example
- * @package PEAR_PackageFileManager
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageFileManager
+ * @author     Greg Beaver <cellog@php.net>
+ * @copyright  2003-2006 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PEAR_PackageFileManager
+ * @since      File available since Release 0.1
  */
-/**
- * Include the package file manager
- */
-require_once('PEAR/PackageFileManager.php');
-$test = new PEAR_PackageFileManager;
-if (PEAR::isError($test)) {
-    echo $test->getMessage();
-    exit;
-}
-$test->setOptions(
-array('baseinstalldir' => 'PEAR',
-'version' => '0.1',
-'packagedirectory' => 'C:/cvsstuff/pear/pear_packagefilemanager',
-'state' => 'alpha',
-'filelistgenerator' => 'file',
-'notes' => 'First release of PEAR_PackageFileManager',
-'ignore' => array('package.xml')));
+
+require_once 'PEAR/PackageFileManager.php';
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$test = new PEAR_PackageFileManager();
+$test->setOptions(array(
+    'package' => 'PEAR_PackageFileManager',
+    'summary' => 'PFM builds package.xml file version 1.0',
+    'description' => 'PFM takes an existing package.xml file and updates it with a new filelist and changelog',
+    'baseinstalldir' => 'PEAR',
+    'version' => '0.1',
+    'packagedirectory' => 'C:/cvsstuff/pear/pear_packagefilemanager',
+    'state' => 'alpha',
+    'filelistgenerator' => 'file',
+    'notes' => 'First release of PEAR_PackageFileManager',
+    'ignore' => array('package.xml')
+    ));
 $test->addDependency('PEAR', '1.1');
-$e = $test->writePackageFile();
-if (PEAR::isError($e)) {
-    echo $e->getMessage();
-}
+$test->addMaintainer('cellog', 'lead', 'Gregory Beaver', 'cellog@php.net');
+$test->writePackageFile();
 ?>
