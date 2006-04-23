6d26dee3a87b9a25d01cd5e1031a544223b4dfbc
Laurent Laville <farell@php.net>
2006-04-23 07:59:22 +0000

    updates package script generator: - add replacements only to necessary files (due to headers comment blocks updates) - updates release note for future beta 1
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager/trunk@211873 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -26,26 +26,31 @@ $packagexml = &PEAR_PackageFileManager2::importOptions(dirname(__FILE__) . DIREC
       'changelogoldtonew' => false,
       'baseinstalldir' => 'PEAR',
       'simpleoutput' => true));
-$packagexml->setNotes('* news
-- add "globalreplaceexceptions" option to exclude files from global replacements
-- add "clearcontents" option to give ability to keep filelist with replaces tasks
-on import function with package v1 or v2
-- fix a notice error with exportCompatiblePackageFile1() method, when you create
-for first time a package.xml v2.0 and want a v1 version.
-Notice: strtotime(): Called with empty time parameter
+$packagexml->setNotes('* bugs fixed
+- #6357: Notice Errors with PFM2 1.6.0a4 and CVS plugin
+- #7393 : addGlobalReplacement() error reporting throws error
+
+* quality assurance
+- updated headers comment block ( http://pear.php.net/pepr/pepr-proposal-show.php?id=128 )
 ');
 $packagexml->addIgnore(array('package.php','*.tgz'));
 $packagexml->setPackageType('php');
 //$packagexml->addMaintainer('lead', 'farell', 'Laurent Laville', 'pear@laurent-laville.org');
 $packagexml->addRelease();
 $packagexml->setChannel('pear.php.net');
-$packagexml->setReleaseVersion('1.6.0a7');
+$packagexml->setReleaseVersion('1.6.0b1');
 $packagexml->setAPIVersion('1.6.0');
 $packagexml->setReleaseStability('alpha');
 $packagexml->setAPIStability('alpha');
 $packagexml->setPhpDep('4.2.0');
 $packagexml->setPearinstallerDep('1.4.3');
 $packagexml->addReplacement('PackageFileManager2.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/File.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/Cvs.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/Perforce.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/Svn.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/SimpleGenerator.php', 'package-info', '@PEAR-VER@', 'version');
+$packagexml->addReplacement('PackageFileManager/XMLOutput.php', 'package-info', '@PEAR-VER@', 'version');
 $packagexml->generateContents();
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
     $packagexml->writePackageFile();
