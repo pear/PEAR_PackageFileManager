c825d64991f097b27f068c05a2ad52c255ecd55c
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2008-12-29 04:02:34 +0000

    I guess I failed at updating the header on this one
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageFileManager/trunk@272167 c90b9560-bf6c-de11-be94-00142212c4b1

--- package_plugins.php
+++ package_plugins.php
@@ -2,17 +2,11 @@
 /**
  * This is the package.xml generator for PEAR_PackageFileManager2
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
  * @category   PEAR
  * @package    PEAR_PackageFileManager
  * @author     Greg Beaver <cellog@php.net>
- * @copyright  2005-2006 The PHP Group
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @copyright  2005-2009 The PEAR Group
+ * @license    New BSD, Revised
  * @version    CVS: $Id$
  * @link       http://pear.php.net/package/PEAR_PackageFileManager
  * @since      File available since Release 1.6.0
